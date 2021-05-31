51e789bf47029cfa24d649c8437bc337c78c8e4b
Nikita126 <mrk.mykyta@gmail.com>
2021-05-31 22:38:41 +0200

    V1.1

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TBL_EMPLOYEES (name, surname, email) VALUES
-('Mykyta', 'Moroka', 'mrk.moroka@gmail.com'),
-('John', 'Doe', 'johndoe@gmail.com'),
-('Micheal', 'Smith', 'micheal33@mail.com');
